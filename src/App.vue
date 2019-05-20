59c564a8dc4153763bd04be5290f2c6be985edd4
pengguangjian <937289279@qq.com>
2019-05-20 14:08:01 +0800

    11

--- src/App.vue
+++ src/App.vue
@@ -4,4 +4,56 @@
   </div>
 </template>
 
-<style lang="less"></style>
+<style lang="less">
+	html{
+		font-size: calc(100vw/7.5); 
+	}
+	
+	body{
+		margin: 0 ;
+    background: #fff;
+    font-size: 14px;
+    font-family: -apple-system,Helvetica,sans-serif;
+    line-height: 1.5;
+    color: #666;
+	}
+	
+	a{
+		text-decoration: none;
+		color: #333;
+	}
+	ul{
+		margin: 0;
+		padding: 0;
+	}
+	
+	ul li{
+		list-style: none;
+	}
+	
+	img{
+		border: 0 none;
+		vertical-align: top;
+	}
+	.left{
+		float: left;
+	}
+	
+	.right{
+		float: right;
+	}
+	
+	.clear{
+		display: block;
+   content: "";
+   clear: both;
+	}
+	
+	.flex{
+		display: flex;
+	}
+	
+	.flex-item{
+		flex:1;
+	}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="home">
-  
+  <search-bar></search-bar>
   </div>
 </template>
 
 <script>
 
+import SearchBar from"@/components/SearchBar"
 
 export default {
   name: 'home',
-  components: {}
+  components: {
+  	SearchBar
+  }
 }
 </script>
